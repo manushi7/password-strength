41cf1eef0aa64d3f1bf803708780ebb948b65142
bishesh bhattarai <bisheshbhattarai@pop-os.localdomain>
2019-12-07 18:52:37 +0545

    new code update

--- passwordstrength.py
+++ passwordstrength.py
@@ -19,3 +19,50 @@ def password_check(pw):
         return value
 pw= input('enter the password : ')
 print(password_check(pw))
+
+
+#-------------------------------------------------------------------------------------------------------------#
+
+
+
+def check_alphanumeric_count(a):
+    chars = ['@', '$', '%', '#', '&', '"', '!', '*', '^', "'"]
+    s = []
+    for i in a:
+        for j in chars:
+            if i == j:
+                s.append(True)
+    if len(s) >= 2:
+        return True
+    else:
+        return False
+
+
+def check_num_count(a):
+    num = [(i+1) for i in range(10)] # [1, 2 , 3, 4, 5 ,6, 7, 8, 9, 10]
+    s = []
+    for i in a:
+        for n in num:
+            if i == str(n):
+                s.append(True)
+    if len(s) >= 2:
+        return True
+    else:
+        return False        
+
+
+def check_password_strength(password):
+    if len(password) >= 7:
+        if check_alphanumeric_count(password) and check_num_count(password):
+            return True
+        else:
+            print("More alphanumeric or num req")
+            return False
+    else:
+        print("Too less characters : MIN 7 required")
+        return False
+
+user_input = input("Please enter your password : ")
+while not check_password_strength(user_input):
+    user_input = input("Please enter another password : ")
+print("Password is good")
